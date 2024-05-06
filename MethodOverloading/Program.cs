6aea5984eef63e679d873ca8fba417f255fd3406
Kaitlyn Thomas <kaitlynthomas2416@gmail.com>
2024-05-06 10:50:28 -0500

    Completed Method Overload Exercise

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,46 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            int addInts = Add(5, 5);
+            Console.WriteLine(addInts);
+
+            decimal addDecimal = Add(9.8m, 5.9m);
+            Console.WriteLine(addDecimal);
+            
+            string addedWithBool = Add(8, 9, true);
+            Console.WriteLine(addedWithBool);
+        }
+
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static decimal Add(decimal x, decimal y)
+        {
+            return x + y;
+        }
+        public static string Add(int x, int y, bool addMoney)
+        {
+            if (addMoney == true)
+            {
+                int total = x + y;
+
+                if (total == 1)
+                {
+                    return $"{total} dollar";
+                }
+
+                else
+                {
+                    return $"{total} dollars";
+                }
+            }
+            else
+            {
+                return "Try Again";
+            }
         }
     }
 }
